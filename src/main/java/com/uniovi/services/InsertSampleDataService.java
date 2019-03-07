0ac053d88d23de1bf860f58cd6a310f0bf53208e
Martin Fernandez <tatin1998@gmail.com>
2019-03-07 12:10:50 +0100

    features: datos minimos de la base de datos

--- src/main/java/com/uniovi/services/InsertSampleDataService.java
+++ src/main/java/com/uniovi/services/InsertSampleDataService.java
@@ -22,6 +22,8 @@ public class InsertSampleDataService {
 	private RolesService rolesService;
 	@Autowired
 	private ConversationService conversationService;
+	@Autowired
+	private BidsService bidService;
 
 	@SuppressWarnings({ "serial", "rawtypes", "unchecked"})
 	@PostConstruct
@@ -41,33 +43,35 @@ public class InsertSampleDataService {
 		User user5 = new User("pelayo@gmail.com", "Pelayo", "Valdes");
 		user5.setPassword("123456");
 		user5.setRole(rolesService.getRoles()[0]);
-		User user6 = new User("edward@gmail.com", "Edward", "Núñez");
-		user6.setPassword("123456");
-		user6.setRole(rolesService.getRoles()[0]);
-		User user7 = new User("admin@email.com", "Administrador", "Administrador");
-		user7.setPassword("admin");
-		user7.setRole(rolesService.getRoles()[1]);
+		User user6 = new User("admin@email.com", "Administrador", "Administrador");
+		user6.setPassword("admin");
+		user6.setRole(rolesService.getRoles()[1]);
 		
+		Bid a1 = new Bid("Oferta A1", "Oferta de prueba A1", 10.0, user1);
+		Bid a2 = new Bid("Oferta A2", "Oferta de prueba A2", 15.0, user1);
+		Bid a3 = new Bid("Oferta A3", "Oferta de prueba A3", 20.0, user1);
+		Bid a4 = new Bid("Oferta A4", "Oferta de prueba A4", 30.0, user1);
+		Bid a5 = new Bid("Oferta A5", "Oferta de prueba A5", 30.0, user1);
 		Bid b1 = new Bid("Oferta B1", "Oferta de prueba B1", 10.2, user2);
-		b1.setBuyerUser(user1);
+		Bid b2 = new Bid("Oferta B2", "Oferta de prueba B2", 15.2, user2);
 		Bid c1 = new Bid("Oferta C1", "Oferta de prueba C1", 10.2, user3);
-		c1.setBuyerUser(user1);
-		Bid d1 = new Bid("Oferta D1", "Oferta de prueba D1", 200.2, user4);
-		d1.setBuyerUser(user2);
+		Bid c2 = new Bid("Oferta C2", "Oferta de prueba C2", 15.2, user3);
+		Bid d2 = new Bid("Oferta D2", "Oferta de prueba D2", 20.2, user4);
 		
 		Set user1Bids = new HashSet<Bid>() {
 			{
-				add(new Bid("Oferta A1", "Oferta de prueba A1", 10.0, user1));
-				add(new Bid("Oferta A2", "Oferta de prueba A2", 15.0, user1));
-				add(new Bid("Oferta A3", "Oferta de prueba A3", 20.0, user1));
-				add(new Bid("Oferta A4", "Oferta de prueba A4", 30.0, user1));
+				add(a1);
+				add(a2);
+				add(a3);
+				add(a4);
+				add(a5);
 			}
 		};
 		user1.setBids(user1Bids);
 		Set user2Bids = new HashSet<Bid>() {
 			{
 				add(b1);
-				add(new Bid("Oferta B2", "Oferta de prueba B2", 15.2, user2));
+				add(b2);
 				add(new Bid("Oferta B3", "Oferta de prueba B3", 20.2, user2));
 				add(new Bid("Oferta B4", "Oferta de prueba B4", 30.2, user2));
 			}
@@ -76,7 +80,7 @@ public class InsertSampleDataService {
 		Set user3Bids = new HashSet<Bid>() {
 			{
 				add(c1);
-				add(new Bid("Oferta C2", "Oferta de prueba C2", 15.2, user3));
+				add(c2);
 				add(new Bid("Oferta C3", "Oferta de prueba C3", 20.2, user3));
 				add(new Bid("Oferta C4", "Oferta de prueba C4", 30.2, user3));
 			}
@@ -84,10 +88,9 @@ public class InsertSampleDataService {
 		user3.setBids(user3Bids);
 		Set user4Bids = new HashSet<Bid>() {
 			{
-				add(d1);
-				add(new Bid("Oferta D2", "Oferta de prueba D2", 20.2, user4));
+				add(new Bid("Oferta D1", "Oferta de prueba D1", 200.2, user4));
+				add(d2);
 				add(new Bid("Oferta D3", "Oferta de prueba D3", 54.2, user4));
-				
 				add(new Bid("Super D1", "Super de prueba D3", 100.0, user4));
 				add(new Bid("Super D2", "Super de prueba D3", 110.0, user4));
 				add(new Bid("Super D3", "Super de prueba D3", 54.2, user4));
@@ -97,6 +100,15 @@ public class InsertSampleDataService {
 			}
 		};
 		user4.setBids(user4Bids);
+		Set user5Bids = new HashSet<Bid>() {
+			{
+				add(new Bid("Oferta E1", "Oferta de prueba E1", 25.2, user5));
+				add(new Bid("Oferta E2", "Oferta de prueba E2", 45.2, user5));
+				add(new Bid("Oferta E3", "Oferta de prueba E3", 23.2, user5));
+				add(new Bid("Oferta E4", "Oferta de prueba E4", 16.23, user5));
+			}
+		};
+		user5.setBids(user5Bids);
 		
 		usersService.addUser(user1);
 		usersService.addUser(user2);
@@ -104,7 +116,6 @@ public class InsertSampleDataService {
 		usersService.addUser(user4);
 		usersService.addUser(user5);
 		usersService.addUser(user6);
-		usersService.addUser(user7);
 		
 		///
 		/// # Conversation producto B1 1
@@ -113,45 +124,60 @@ public class InsertSampleDataService {
 			{
 				add(new Message(conversationB1_1, user1, new GregorianCalendar(2019, 2, 6, 16, 00, 10).getTime(), "B1_1 - Hola, estoy interesado en el producto."));
 				add(new Message(conversationB1_1, user2, new GregorianCalendar(2019, 2, 6, 16, 00, 11).getTime(), "B1_1 - Si te gusta compralo."));
-				add(new Message(conversationB1_1, user1, new GregorianCalendar(2019, 2, 6, 16, 00, 12).getTime(), "B1_1 - Vale, lo hare :D"));
-				add(new Message(conversationB1_1, user1,new GregorianCalendar(2019, 2, 6, 16, 00, 13).getTime(), "B1_1 - Gracias."));
+				add(new Message(conversationB1_1, user2, new GregorianCalendar(2019, 2, 6, 16, 00, 12).getTime(), "B1_1 - Esta muy barato."));
+				add(new Message(conversationB1_1, user1, new GregorianCalendar(2019, 2, 6, 16, 00, 13).getTime(), "B1_1 - Vale, lo hare :D"));
+				add(new Message(conversationB1_1, user1,new GregorianCalendar(2019, 2, 6, 16, 00, 15).getTime(), "B1_1 - Gracias."));
 			}
 		};
 		conversationB1_1.setMessages(messagesProductB1_1);
 		
 		/// # Conversation producto B1 2
 		Conversation conversationB1_2 = new Conversation(user3, b1);
-		Message conversationB1_2_message1 = new Message(conversationB1_2, user3, new GregorianCalendar(2019, 2, 6, 16, 11, 10).getTime(), "B1_2 - Hola, estoy interesado en el producto.");
-		Message conversationB1_2_message2 = new Message(conversationB1_2, user2, new GregorianCalendar(2019, 2, 6, 16, 11, 11).getTime(), "B1_2 - Si te gusta compralo.");
-		Message conversationB1_2_message3 = new Message(conversationB1_2, user3, new GregorianCalendar(2019, 2, 6, 16, 11, 12).getTime(), "B1_2 - Vale, lo hare :D");
-		Message conversationB1_2_message4 = new Message(conversationB1_2, user3, new GregorianCalendar(2019, 2, 6, 16, 11, 13).getTime(), "B1_2 - Gracias.");
-		
 		Set messagesProductB1_2 = new HashSet<Message>() {
 			{
-				add(conversationB1_2_message1);
-				add(conversationB1_2_message2);
-				add(conversationB1_2_message3);
-				add(conversationB1_2_message4);
+				add(new Message(conversationB1_2, user3, new GregorianCalendar(2019, 2, 6, 16, 11, 10).getTime(), "B1_2 - Hola, estoy interesado en el producto."));
+				add(new Message(conversationB1_2, user2, new GregorianCalendar(2019, 2, 6, 16, 11, 11).getTime(), "B1_2 - Si te gusta compralo."));
+				add(new Message(conversationB1_2, user2, new GregorianCalendar(2019, 2, 6, 16, 11, 12).getTime(), "B1_2 - Esta muy barato."));
+				add(new Message(conversationB1_2, user3, new GregorianCalendar(2019, 2, 6, 16, 11, 13).getTime(), "B1_2 - Vale, lo hare :D"));
+				add(new Message(conversationB1_2, user3, new GregorianCalendar(2019, 2, 6, 16, 11, 14).getTime(), "B1_2 - Gracias."));
 			}
 		};
 		conversationB1_2.setMessages(messagesProductB1_2);
 		
 		/// # Conversation producto C1 1
 		Conversation conversationC1_1 = new Conversation(user1, c1);
-		Message conversationC1_2_message1 = new Message(conversationC1_1, user1, new GregorianCalendar(2019, 2, 6, 16, 12, 10).getTime(), "C1_1 - Hola, estoy interesado en el producto.");
-		Message conversationC1_2_message2 = new Message(conversationC1_1, user3, new GregorianCalendar(2019, 2, 6, 16, 12, 11).getTime(), "C1_1 - Si te gusta compralo.");
-		Message conversationC1_2_message3 = new Message(conversationC1_1, user1, new GregorianCalendar(2019, 2, 6, 16, 12, 12).getTime(), "C1_1 - Vale, lo hare :D");
-		Message conversationC1_2_message4 = new Message(conversationC1_1, user1, new GregorianCalendar(2019, 2, 6, 16, 12, 13).getTime(), "C1_1 - Gracias.");
-		
 		Set messagesProductC1_1 = new HashSet<Message>() {
 			{
-				add(conversationC1_2_message1);
-				add(conversationC1_2_message2);
-				add(conversationC1_2_message3);
-				add(conversationC1_2_message4);
+				add(new Message(conversationC1_1, user1, new GregorianCalendar(2019, 2, 6, 16, 12, 10).getTime(), "C1_1 - Hola, estoy interesado en el producto."));
+				add(new Message(conversationC1_1, user3, new GregorianCalendar(2019, 2, 6, 16, 12, 11).getTime(), "C1_1 - Si te gusta compralo."));
+				add(new Message(conversationC1_1, user3, new GregorianCalendar(2019, 2, 6, 16, 11, 12).getTime(), "B1_2 - Esta muy barato."));
+				add(new Message(conversationC1_1, user1, new GregorianCalendar(2019, 2, 6, 16, 12, 13).getTime(), "C1_1 - Vale, lo hare :D"));
+				add(new Message(conversationC1_1, user1, new GregorianCalendar(2019, 2, 6, 16, 12, 14).getTime(), "C1_1 - Gracias."));
 			}
 		};
 		conversationC1_1.setMessages(messagesProductC1_1);
+		
+		a1.setBuyerUser(user2);
+		a2.setBuyerUser(user4);
+		a3.setBuyerUser(user5);
+		a4.setBuyerUser(user4);
+		a5.setBuyerUser(user5);
+		b1.setBuyerUser(user1);
+		b2.setBuyerUser(user3);
+		c1.setBuyerUser(user1);
+		c2.setBuyerUser(user3);
+		d2.setBuyerUser(user2);
+		
+		bidService.addBid(a1);
+		bidService.addBid(a2);
+		bidService.addBid(a3);
+		bidService.addBid(a4);
+		bidService.addBid(a5);
+		bidService.addBid(b1);
+		bidService.addBid(b2);
+		bidService.addBid(c1);
+		bidService.addBid(c2);
+		bidService.addBid(d2);
 		
 		conversationService.addConversation(conversationB1_1);
 		conversationService.addConversation(conversationB1_2);
