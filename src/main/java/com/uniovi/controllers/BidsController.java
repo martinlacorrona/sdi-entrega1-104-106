111f93b54b49e3ad9845e0e600ffc3430c2f2c1e
martinlacorrona <tatin1998@gmail.com>
2019-03-13 10:00:19 +0100

    fix: arreglado el mensaje de no tener dinero y compra realizada

--- src/main/java/com/uniovi/controllers/BidsController.java
+++ src/main/java/com/uniovi/controllers/BidsController.java
@@ -103,6 +103,16 @@ public class BidsController {
 		model.addAttribute("bidList", bids.getContent());
 		model.addAttribute("page", bids);
 		model.addAttribute("urlPath", searchText);
+		
+		if(request.getSession().getAttribute("error") != null) {
+			model.addAttribute("error", "true");
+			request.getSession().setAttribute("error", null); 
+		}
+		
+		if(request.getSession().getAttribute("buyed") != null) {
+			model.addAttribute("buyed", "true");
+			request.getSession().setAttribute("buyed", null); 
+		}
 
 		return "bid/list";
 	}
@@ -131,9 +141,8 @@ public class BidsController {
 			seller.setMoney(finalMoneySeller);
 			usersService.updateMoney(finalMoneySeller, seller.getEmail());
 
-			// Reiniciamos
-			request.getSession().setAttribute("error", null);
-			request.getSession().setAttribute("id", id);
+			//Atributo de comprado a la sesion
+			request.getSession().setAttribute("buyed", "true");
 		} else {
 			request.getSession().setAttribute("error", "Error.buy");
 			request.getSession().setAttribute("id", id);
